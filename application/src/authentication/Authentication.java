c35e958715aace6bc806f53daba0d8c573dfbcb3
Edwin Oliveira <39379560+EdwinOliveira@users.noreply.github.com>
2020-03-12 15:12:00 +0000

    Added some comentary for my fellow reporters

--- application/src/authentication/Authentication.java
+++ application/src/authentication/Authentication.java
@@ -17,6 +17,10 @@ public class Authentication {
         
     }
 
+    /** 
+     * This function will return the UNIQUE instance to ever be created on this project (UNIQUE/GLOBAL)
+     * @return Authentication Class Instance
+     */
     public Authentication getAuthenticationInstance() {
         if(this.authenticationInstance == null)
             this.authenticationInstance = new Authentication();
@@ -24,12 +28,23 @@ public class Authentication {
         return this.authenticationInstance;
     }
 
+    /**
+     * This function will create a new Driver so it can be authenticated
+     * @param name
+     * @param password
+     */
     public void createDriver(String name, String password) {
         Driver driver = new Driver(name, password);
         
         this.driversHash.put((this.driversHash.size() + 1), driver);  
     }
 
+    /**
+     * This function will verify if the given argumments are stored on the Driver class
+     * @param name
+     * @param password
+     * @return
+     */
     public Boolean checkDriver(String name, String password) {
         for(Driver driver : this.driversHash.values()) {
             if((driver.getName() == name) && (driver.getPassword() == password))
